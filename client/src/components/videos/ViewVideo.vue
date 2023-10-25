bd51d3a441db03424d19b2a334c8784ff521ca07
Soubhik <gsoubhik01@gmail.com>
2023-10-25 09:38:35 +0530

    Added comment date

--- client/src/components/videos/ViewVideo.vue
+++ client/src/components/videos/ViewVideo.vue
@@ -61,6 +61,7 @@
               <div class="comment-item">
                 <span class="comment-username">{{ comment.username }}</span>
                 <p class="comment-content">{{ comment.content }}</p>
+                <p class="comment-date right-align">{{ formattedCommentDate(comment.comment_date) }}</p>
               </div>
             </v-list-item>
           </v-list>
@@ -182,6 +183,37 @@ export default {
         return `${secondsAgo} second${secondsAgo > 1 ? "s" : ""} ago`;
       }
     },
+    formattedCommentDate() {
+    return (date) => {
+      if (!date) {
+        return "";
+      }
+
+      const uploadDate = new Date(date);
+      const currentDate = new Date();
+      const timeDifference = currentDate - uploadDate;
+      const secondsAgo = Math.floor(timeDifference / 1000);
+      const minutesAgo = Math.floor(secondsAgo / 60);
+      const hoursAgo = Math.floor(minutesAgo / 60);
+      const daysAgo = Math.floor(hoursAgo / 24);
+      const monthsAgo = Math.floor(daysAgo / 30.42);
+      const yearsAgo = Math.floor(monthsAgo / 12);
+
+      if (yearsAgo > 0) {
+        return `${yearsAgo} year${yearsAgo > 1 ? "s" : ""} ago`;
+      } else if (monthsAgo > 0) {
+        return `${monthsAgo} month${monthsAgo > 1 ? "s" : ""} ago`;
+      } else if (daysAgo > 0) {
+        return `${daysAgo} day${daysAgo > 1 ? "s" : ""} ago`;
+      } else if (hoursAgo > 0) {
+        return `${hoursAgo} hour${hoursAgo > 1 ? "s" : ""} ago`;
+      } else if (minutesAgo > 0) {
+        return `${minutesAgo} minute${minutesAgo > 1 ? "s" : ""} ago`;
+      } else {
+        return `${secondsAgo} second${secondsAgo > 1 ? "s" : ""} ago`;
+      }
+    };
+  },
   },
 
   methods: {
@@ -445,4 +477,12 @@ export default {
 .comment-action{
   margin:10px;
 }
+.comment-date {
+  color: #777;
+  font-size: 12px;
+}
+.comment-date.right-align {
+  text-align: right;
+}
+
 </style>
